96c364d757951a617d4b4394b45c313b040fb270
Ashique Mohaimin <amornab@gmail.com>
2018-07-05 11:31:36 +0600

    First commit

---
+++ app/src/main/java/com/hfad/beeradvisor/MainActivity.java
@@ -0,0 +1,43 @@
+package com.hfad.beeradvisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickFindBeer(View view)
+    {
+         TextView brands = (TextView)findViewById(R.id.brands);
+
+         Spinner color = (Spinner)findViewById(R.id.color);
+
+         String beertype = String.valueOf(color.getSelectedItem());
+
+        BeerExpert expert = new BeerExpert();
+        List<String> brandslist = expert.getBrands(beertype);
+
+        String output = "";
+
+        for(int i = 0; i < brandslist.size(); i++)
+        {
+            output = output + "\n" + brandslist.get(i);
+
+        }
+
+        brands.setText(output);
+
+
+    }
+}
